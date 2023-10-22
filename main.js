516606a522609e5d02c27843f7ce9dac96bffd19
Poom Khorchitmate <poom.khor@gmail.com>
2023-10-22 20:57:51 +0700

    add variable and init to main.js

--- main.js
+++ main.js
@@ -11,3 +11,35 @@ console.log('This is working!');
 // 8. continue until either the player found all the matches or times out or attempts been maxed out at 70
 // 9. if found all match, display mssg "You Won, Next Level" and continue to next level with shorter card hang time. If no next level, display mssg "You Made It ALL!, What a GENIUS!! Your Total Score is ${}"
 // 10. if not won, display mssg "You Lose!! Hit Restart Again"
+
+// declare variables
+let win;
+let level;
+let attempt;
+let match;
+
+// accessing the DOM element during event listener ('click')
+// cache HTML element
+const cardElement = document.querySelector('.card-container');
+const resetElement = document.querySelector('button');
+
+// initialize the game state --> 'MODEL'
+
+init();
+
+function init() {
+    win = undefined;
+    level = 1;
+    attempt = 0;
+    match = 0;
+    // game init state
+    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+}
+
+// add 'CONTROLLER' to listen to users action and set function that should manipulate the model
+cardElement.addEventListener('click', function (event) {
+    event.preventDefault();
+    return;
+    // flip card and check if two card is opened?
+    //
+});
